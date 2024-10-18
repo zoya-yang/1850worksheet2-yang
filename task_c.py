7a996ad411d79dd1c1aad1019a6c484d30003088
zoya-yang <15563928777@163.com>
2024-10-18 02:08:35 +0100

    Update task_c.py

--- task_c.py
+++ task_c.py
@@ -1,9 +1,9 @@
 password = input("Please enter a password: ")
-is_long_enough = len(password) >=8
+is_long_enough = len(password) >= 8
 contains_letter = any(char.isalpha() for char in password)
 contains_number = any(char.isdigit() for char in password)
 
 if is_long_enough and contains_letter and contains_number:
     print("Your password is valid")
 else:
-    print("YOur password must contain at lease 8 characters, and mix of letters and numberes")
+    print("Your password must contain at least 8 characters, and mix of letters and numbers")
